75e70fcc6c87f4ffd77a2c56ac16291224ce4e76
Arvin Buid <arvinbuid01@gmail.com>
2025-10-03 17:26:21 +0800

    feat: implement remove method to bst

--- bst.js
+++ bst.js
@@ -76,6 +76,76 @@ class BinarySearchTree {
     }
     return false;
   }
+
+  remove(val) {
+    let target = this.root;
+    let parent;
+
+    // Find the target node, ASSIGN PARENT NODE
+    while (target.value !== val) {
+      parent = target;
+      if (val < target.value) {
+        target = target.left;
+      } else {
+        target = target.right;
+      }
+    }
+
+    // Check if target is not the root
+    if (target !== this.root) {
+      // If no child nodes
+      if (target.left === null && target.right === null) {
+        if (parent.left === target) {
+          parent.left = null;
+        } else {
+          parent.right = null;
+        }
+        // If two children nodes
+      } else if (target.left !== null && target.right !== null) {
+        let rightParent = target;
+        let right = target.right;
+        if (right.left === null) {
+          right.left = target.left;
+          if (parent.left === target) {
+            parent.left = right;
+          } else {
+            parent.right = right;
+          }
+        } else {
+          while (right.left !== null) {
+            rightParent = right;
+            right = right.left;
+          }
+          if (parent.left === target) {
+            parent.left.value = right.value;
+          } else {
+            parent.right.value = right.value;
+          }
+          if (right.right !== null) {
+            rightParent.left = right.right;
+          } else {
+            rightParent.left = null;
+          }
+        }
+        // If one child node only
+      } else {
+        if (parent.left === target) {
+          if (target.right === null) {
+            parent.left = target.left;
+          } else {
+            parent.left = target.right;
+          }
+        } else {
+          if (target.right === null) {
+            parent.right = target.left;
+          } else {
+            parent.right = target.right;
+          }
+        }
+      }
+    }
+    return target;
+  }
 }
 
 const bst = new BinarySearchTree();
@@ -88,6 +158,9 @@ bst.insert(14);
 bst.insert(8);
 bst.insert(11);
 console.log(bst.contains(10));
+bst.remove(14);
+bst.remove(11);
+console.log(bst);
 
 //  Big O:
 //  Insertion - O(log n)
