dae0390cb5f0f7125882ecf0c0f295b4a54d1806
Arvin Buid <arvinbuid01@gmail.com>
2025-10-06 00:41:58 +0800

    chore: add graph terms and types of graphs notes

--- graphs.js
+++ graphs.js
@@ -1,4 +1,5 @@
-// Graphs - a graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of //unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graphs.
+// Graphs - a graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points,
+//          together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graphs
 //        - nodes + collections
 
 // Uses for Graphs: Social Networks
@@ -6,3 +7,16 @@
 //                - Routing Algorithms
 //                - Visual Hierarchy
 //                - File System Optimizations
+
+// Graph Terms:
+//
+// 1.) Vertex - a node
+// 2.) Edge - connection between nodes
+
+//
+// Types of Graphs:
+//
+// 1.) Undirected Graph - there is no direction or polarity to the connections of edges between the vertices
+// 2.) Directed Graph - often represented with arrows which indicates the direction and polarity assign to an edge
+// 3.) Weighted Graph - graph where each edge has a number (weight) that represents distance, cost, or time
+// 4.) Unweighted Graph - opposite of weighted graph
