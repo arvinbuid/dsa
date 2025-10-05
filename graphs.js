867fdf2e9f264c554bb1ca26daec05a9c5c241ee
Arvin Buid <arvinbuid01@gmail.com>
2025-10-05 23:34:38 +0800

    chore: create graphs.js and add definition, graphs use-case notes

---
+++ graphs.js
@@ -0,0 +1,8 @@
+// Graphs - a graph data structure consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of //unordered pairs of these vertices for an undirected graph or a set of ordered pairs for a directed graphs.
+//        - nodes + collections
+
+// Uses for Graphs: Social Networks
+//                - Location/Mapping
+//                - Routing Algorithms
+//                - Visual Hierarchy
+//                - File System Optimizations
