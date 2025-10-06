b44cf71681093ea88e9fdd262fa74c2e5a6887a5
Arvin Buid <arvinbuid01@gmail.com>
2025-10-06 22:05:48 +0800

    chore: create graph-traversal.js and add notes

---
+++ graph-traversal.js
@@ -0,0 +1,12 @@
+/* Visiting/Updating/Checking each vertex in a Graph */
+//
+//
+// Graph Traversal Use Cases:
+//
+// - Peer-to-peer networking
+// - Web crawlers
+// - Finding 'closest' matches/recommendations
+// - Shortest path problems
+//      - GPS Navigation
+//      - Solving mazes
+//      - AI (shortest path to win the game)
