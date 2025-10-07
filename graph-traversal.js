a73011930d453d7c5bdf5f1436d1d90136045825
Arvin Buid <arvinbuid01@gmail.com>
2025-10-07 12:25:59 +0800

    note: graph dfs traversal definition and pseudocode

--- graph-traversal.js
+++ graph-traversal.js
@@ -10,3 +10,18 @@
 //      - GPS Navigation
 //      - Solving mazes
 //      - AI (shortest path to win the game)
+
+// Graph Depth First Search (DFS) - we traverse all adjacent vertices one by one.
+//                                - When we traverse an adjacent vertex, we completely finish the traversal of
+//                                  all vertices reachable through that adjacent vertex.
+
+// DFS Graph Pseudocode:
+//
+// DFS(vertex)
+//    if vertex is empty
+//        return - (base case)
+//    add vertex to results list
+//    mark vertex as visited
+//    for each neighbor in vertex's neighbors:
+//        if neighbor is not visited:
+//           recursively call DFS on neighbor
