40696d63356d95cc1e2306d651d5a2e4045cf2a4
Arvin Buid <arvinbuid01@gmail.com>
2025-10-07 17:59:51 +0800

    feat: add Graph class in graph-traversal.js and implement depthFirstRecursive

--- graph-traversal.js
+++ graph-traversal.js
@@ -25,3 +25,83 @@
 //    for each neighbor in vertex's neighbors:
 //        if neighbor is not visited:
 //           recursively call DFS on neighbor
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    // set the adjacency list of that key to empty []
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      while (this.adjacencyList[vertex].length) {
+        const adjacentVertex = this.adjacencyList[vertex].pop(); // remove each values inside of the vertex
+        this.removeEdge(vertex, adjacentVertex);
+      }
+      delete this.adjacencyList[vertex]; // delete the vertex
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) return false;
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) return false;
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1);
+  }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    // helper function (recursive)
+    (function dfs(vertex) {
+      if (!vertex) return null; // base case
+      visited[vertex] = true; // place the vertex into visited object
+      result.push(vertex);
+
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor); // if values not visited, recursively invoke dfs with the vertex
+        }
+      });
+    })(start);
+
+    return result;
+  }
+}
+
+const g = new Graph();
+
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+console.log(g.depthFirstRecursive("A"));
+
+//           A
+//         /   \
+//        B     C
+//        \     /
+//         D---E
+//          \ /
+//           F
