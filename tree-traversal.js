fc2581f6d5c3bae522d873f95012e4108aca13a1
Arvin Buid <arvinbuid01@gmail.com>
2025-10-08 18:37:17 +0800

    chore: create tree-traversal.js and add notes

---
+++ tree-traversal.js
@@ -0,0 +1,7 @@
+/* Two ways of traversing a tree - Breadth-first Search, Depth-first Search */
+
+// Depth-first Traversals:
+//
+// Inorder Traversal (Left-Root-Right)
+// Preorder Traversal (Root-Left-Right)
+// Postorder Traversal (Left-Right-Root)
