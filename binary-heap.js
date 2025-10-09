2d924dc7f13be8b0857c1fa948d1ff0213d98339
Arvin Buid <arvinbuid01@gmail.com>
2025-10-09 23:50:05 +0800

    feat: implement extractMax() on BinaryHeap class

--- binary-heap.js
+++ binary-heap.js
@@ -12,9 +12,10 @@
 // What if there is a child node and want to find its parent? - for any child node at index n,
 //                                                            - its parent is at index (n-1)/2 floored (round down decimal place)
 //
+// This is a Max Binary Heap implementation
 class BinaryHeap {
   constructor() {
-    this.values = [41, 39, 33, 18, 27, 12];
+    this.values = [55, 41, 39, 33, 18, 27, 12];
   }
   insert(element) {
     this.values.push(element);
@@ -32,11 +33,57 @@ class BinaryHeap {
       index = parentIdx;
     }
   }
+
+  // equivalent to remove
+  // remove the root -> replace with the most recently added
+  // adjust (sink down)
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  // [ 55, 41, 39, 33, 18, 27, 12 ]
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      // Check the left side if not out of bounds, then do swap
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      // Check the right side if not out of bounds, then do swap
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+
+        // The logic of this if statement is tough for me to understand for quite a while:<
+        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break; // if there is no swap available, break out of the loop
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
 
 const heap = new BinaryHeap();
-heap.insert(55);
-heap.insert(299);
+console.log(heap.extractMax());
 console.log(heap);
-// [41, 39, 33, 18, 27, 12, 55]
-//  0   1   2   3   4   5   6
