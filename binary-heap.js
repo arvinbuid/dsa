55fc85701d612e46f7b8fa9fb0a8f73005977521
Arvin Buid <arvinbuid01@gmail.com>
2025-10-09 15:54:40 +0800

    chore: create binary-heap.js and add notes

---
+++ binary-heap.js
@@ -0,0 +1,7 @@
+// Binary Heap - complete binary tree that stores data efficiently, allowing quick access
+//               to the maximum or minimum element, depending on the type of heap
+//
+// Types of Binary Heap:
+//
+// 1.) Max Binary Heap - parent nodes are always larger than the child nodes but not guarantee between sibling nodes
+// 2.) Min Binary Heap - parent nodes are always smaller than the child nodes, also not guarantee between sibling nodes
