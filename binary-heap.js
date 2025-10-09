4c91dc77fe452d9ed26788e37a3605b2d5365102
Arvin Buid <arvinbuid01@gmail.com>
2025-10-09 18:25:11 +0800

    notes: storing heaps

--- binary-heap.js
+++ binary-heap.js
@@ -5,3 +5,9 @@
 //
 // 1.) Max Binary Heap - parent nodes are always larger than the child nodes but not guarantee between sibling nodes
 // 2.) Min Binary Heap - parent nodes are always smaller than the child nodes, also not guarantee between sibling nodes
+//
+// Storing Heaps - for any index of an array n...
+//               - the left child is stored at 2n+1
+//               - the right child is at 2n+2
+// What if there is a child node and want to find its parent? - for any child node at index n,
+//                                                            - its parent is at index (n-1)/2 floored (round down decimal place)
