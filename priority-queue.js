aefe79eced180330f53241838f3d60468dc63546
Arvin Buid <arvinbuid01@gmail.com>
2025-10-10 16:15:29 +0800

    feat: update PriorityQueue class

--- priority-queue.js
+++ priority-queue.js
@@ -12,19 +12,20 @@ class Node {
 
 class PriorityQueue {
   constructor() {
-    this.values = [55, 41, 39, 33, 18, 27, 12];
+    this.values = [];
   }
-  insert(element) {
-    this.values.push(element);
+  enqueue(value, priority) {
+    const newNode = new Node(value, priority);
+    this.values.push(newNode);
     this.bubbleUp();
   }
   bubbleUp() {
-    let index = this.values.length - 1; // last element in the array
+    let index = this.values.length - 1;
     const element = this.values[index];
     while (index > 0) {
       let parentIdx = Math.floor((index - 1) / 2);
       let parent = this.values[parentIdx];
-      if (element <= parent) break; // stop the loop
+      if (element.priority >= parent.priority) break;
       this.values[parentIdx] = element;
       this.values[index] = parent;
       index = parentIdx;
@@ -34,17 +35,16 @@ class PriorityQueue {
   // equivalent to remove
   // remove the root -> replace with the most recently added
   // adjust (sink down)
-  extractMax() {
-    const max = this.values[0];
+  dequeue() {
+    const min = this.values[0]; // update the variable into min
     const end = this.values.pop();
     if (this.values.length > 0) {
       this.values[0] = end;
       this.sinkDown();
     }
-    return max;
+    return min;
   }
 
-  // [ 55, 41, 39, 33, 18, 27, 12 ]
   sinkDown() {
     let index = 0;
     const length = this.values.length;
@@ -58,7 +58,7 @@ class PriorityQueue {
       // Check the left side if not out of bounds, then do swap
       if (leftChildIndex < length) {
         leftChild = this.values[leftChildIndex];
-        if (leftChild > element) {
+        if (leftChild.priority < element.priority) {
           swap = leftChildIndex;
         }
       }
@@ -67,8 +67,11 @@ class PriorityQueue {
       if (rightChildIndex < length) {
         rightChild = this.values[rightChildIndex];
 
-        // The logic of this if statement is tough for me to understand for quite a while:<
-        if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+        // Now comparison is based on the elements priority
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
           swap = rightChildIndex;
         }
       }
@@ -81,4 +84,22 @@ class PriorityQueue {
   }
 }
 
-const pq = new PriorityQueue();
+// Hospital Queue
+const hq = new PriorityQueue();
+hq.enqueue("fever", 2);
+hq.enqueue("diarrhea", 3);
+hq.enqueue("stroke", 1);
+hq.enqueue("trauma", 1);
+hq.enqueue("trouble breathing", 2);
+hq.enqueue("slurred speech", 4);
+console.log(hq.dequeue());
+console.log(hq.dequeue());
+console.log(hq.dequeue());
+console.log(hq); // Results at the end:
+//                                      PriorityQueue {
+//                                        values: [
+//                                          Node { value: 'fever', priority: 2 },
+//                                          Node { value: 'diarrhea', priority: 3 },
+//                                          Node { value: 'slurred speech', priority: 4 }
+//                                        ]
+//                                      }
