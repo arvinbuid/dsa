3c65edad2893158a951af3293cd8b62b31f4bd25
Arvin Buid <arvinbuid01@gmail.com>
2025-10-11 14:36:55 +0800

    chore: create simple-priority-queue.js and implement SimplePriorityQueue class

--- dijkstra's-algorithm.js
+++ dijkstra's-algorithm.js
@@ -1,5 +1,6 @@
 // Dijkstra's Algorithm - finds the shortest path between two vertices on a graph
-//                      - what is the fastest way to get from point A to point B                   - what is the fastest way to get from point A to point B
+//                      - what is the fastest way to get from point A to point B
+//                      - what is the fastest way to get from point A to point B
 
 class WeightedGraph {
   constructor() {
