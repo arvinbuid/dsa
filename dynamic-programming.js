999011ef446141d16ad9989a736b0d28f2d33a5b
Arvin Buid <arvinbuid01@gmail.com>
2025-10-12 23:38:08 +0800

    notes: tabulation

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -3,10 +3,10 @@
 // Conditions:
 //
 // 1.) Overlapping Subproblems - a problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times
-//                         - ex. Fibonacci Sequence
+//                             - ex. Fibonacci Sequence
 
 // 2.) Optimal Substructure - a problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
-//                         - ex. Fibonacci Sequence, Shortest Path between vertices on a Graph
+//                          - ex. Fibonacci Sequence, Shortest Path between vertices on a Graph
 
 // Fibonacci Sequence Recursive Code:
 function fibonacci(n) {
@@ -22,8 +22,8 @@ function fibonacci(n) {
 // To solve the issue about fibonacci recursive approach - 'use past knowledge to make solving future problem easier.'
 //                                                       - use Memoization approach
 
-// Memoization - storing the results of expensive function calls and returning
-//               the cached result when the same inputs occur again
+// Memoization (Top-Down Approach) - storing the results of expensive function calls and returning
+//                                   the cached result when the same inputs occur again
 
 // Memoization Approach
 function improvedFibonacci(n, memo = []) {
@@ -37,3 +37,7 @@ console.log(improvedFibonacci(100));
 // Big O Notation: O(n) - Big O Linear Time
 //                      - the improved fibonacci is much faster and efficient
 //                      - as (n) grows, the time it takes for this algorithm to run grows in proportion with (n)
+
+// Tabulation (Bottom-Up Approach) - storing the result of a previous result in a 'table' (usually an array)
+//                                 - usually done using iteration
+//                                 - better space complexity can be achieved using tabulation
