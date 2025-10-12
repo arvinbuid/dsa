ae41f7b49eff38103c071ced15eb12f07c6cb396
Arvin Buid <arvinbuid01@gmail.com>
2025-10-12 17:25:18 +0800

    notes: overlapping problems and optimal substructure

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -1,1 +1,9 @@
 // Dynamic Programming - a method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions.
+
+// Conditions:
+//
+// 1.) Overlapping Subproblems - a problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times
+//                         - ex. Fibonacci Sequence
+
+// 2.) Optimal Substructure - a problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
+//                         - ex. Fibonacci Sequence, Shortest Path between vertices on a Graph
