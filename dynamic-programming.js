6e901a64d931b693226431eb53d079ffb6080247
Arvin Buid <arvinbuid01@gmail.com>
2025-10-12 17:52:47 +0800

    feat: create fibonacci() and add big o notation notes

--- dynamic-programming.js
+++ dynamic-programming.js
@@ -7,3 +7,14 @@
 
 // 2.) Optimal Substructure - a problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
 //                         - ex. Fibonacci Sequence, Shortest Path between vertices on a Graph
+
+// Fibonacci Sequence Recursive Code:
+function fibonacci(n) {
+  if (n <= 2) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(25));
+
+// Big O Notation: O(2^n) - Big O Exponential
+//                        - the time complexity is terrible and very slow (need improvement)
