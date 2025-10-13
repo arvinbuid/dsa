060c522cc6abe8e6e8a8d8206a46a01ef0b3cbab
Arvin Buid <arvinbuid01@gmail.com>
2025-10-13 19:05:44 +0800

    chore: create singly-linked-list.js and add notes

---
+++ singly-linked-list.js
@@ -0,0 +1,10 @@
+// Linked list - a data structure that contains a head, tail and length property
+//             - consists of nodes, and each node has a value and a pointer to another node or null
+//             - do not have indexes
+//             - connected via nodes with a next pointer
+//             - random access is not allowed
+//             - excellent at insertion and deletion Big O(1) Constant Time
+
+// Singly linked list - it consists of nodes where each node contains a data field and a reference to the next node in the linked list
+//                    - the next of the last node is null, indicating the end of the list
+//                    - each node consists of two parts: data and a pointer to the next node
