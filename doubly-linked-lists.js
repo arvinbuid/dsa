173479aeff0e3e555f9f3e1082c5e73b5ece8b18
Arvin Buid <arvinbuid01@gmail.com>
2025-10-17 01:34:45 +0800

    chore: create doubly-linked-lists.js and add notes

---
+++ doubly-linked-lists.js
@@ -0,0 +1,3 @@
+// Doubly Linked Lists - almost identical to Singly linked lists, except every node has
+//                       another pointer, to the previous node and a pointer to the next node
+//                     - it needs more memory than Singly linked lists
