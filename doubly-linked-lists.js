9d2d4f396c87bd8d9f92ef2fb7e2eb1d58197bb0
Arvin Buid <arvinbuid01@gmail.com>
2025-10-17 15:51:26 +0800

    feat: create Node and DoublyLinkedList class

--- doubly-linked-lists.js
+++ doubly-linked-lists.js
@@ -1,3 +1,19 @@
 // Doubly Linked Lists - almost identical to Singly linked lists, except every node has
 //                       another pointer, to the previous node and a pointer to the next node
 //                     - it needs more memory than Singly linked lists
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
