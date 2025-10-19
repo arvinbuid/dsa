0c89fd9ff14309972107f0f62ae5f575e954c3ca
Arvin Buid <arvinbuid01@gmail.com>
2025-10-19 22:01:21 +0800

    feat: create Node and Stack class on stack.js

--- stack.js
+++ stack.js
@@ -1,3 +1,20 @@
 // Stack - a LIFO data structure
 //       - the last element added to the stack will be the first element removed from the stack
 //       - stacks are used in managing function invocation, undo/redo, routing (history object)
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push() {}
+}
