aa92ee732be9a590186637b6695a314df458cce6
Arvin Buid <arvinbuid01@gmail.com>
2025-10-19 19:01:10 +0800

    chore: create stack.js and add notes

---
+++ stack.js
@@ -0,0 +1,3 @@
+// Stack - a LIFO data structure
+//       - the last element added to the stack will be the first element removed from the stack
+//       - stacks are used in managing function invocation, undo/redo, routing (history object)
