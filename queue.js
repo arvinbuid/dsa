9d4359ee10df2205c3e4edcbba038ba83b16040d
Arvin Buid <arvinbuid01@gmail.com>
2025-10-19 23:09:17 +0800

    chore: create queue.js and implement Node and Queue class

---
+++ queue.js
@@ -0,0 +1,17 @@
+// Queue - a FIFO data structure
+//       - used in programming for background tasks, uploading resources, printing/task processing
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
