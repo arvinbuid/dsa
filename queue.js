82cd3b913be247081bdae172e13eeb5cb086b5d0
Arvin Buid <arvinbuid01@gmail.com>
2025-10-20 14:19:16 +0800

    feat: implement enqueue() on Queue class

--- queue.js
+++ queue.js
@@ -14,4 +14,23 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+  }
 }
+
+const queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+console.log(queue);
